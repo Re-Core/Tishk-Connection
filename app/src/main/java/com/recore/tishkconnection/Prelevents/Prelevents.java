0be963e97c042ea1c3310efe23904aba29597b99
rebin <rebinaziz21@gmail.com>
2019-05-03 22:29:51 +0300

    pre-alpha v5 now u can look at people profile

--- app/src/main/java/com/recore/tishkconnection/Prelevents/Prelevents.java
+++ app/src/main/java/com/recore/tishkconnection/Prelevents/Prelevents.java
@@ -4,8 +4,6 @@ import com.recore.tishkconnection.Model.User;
 
 public class Prelevents {
 
-    public static final String UserIdKey = "UserIdKey";
-    public static final String UserPasswordKey = "UserPassword";
     public static User currentOnlineUser;
 
 
